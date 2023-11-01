d2e489b317d9619667ae516958033b8cec6c0672
Jenae Janzen <jenae.janzen@netlify.com>
2023-11-01 16:13:32 -0400

    add content type filter

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,8 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST
+        host: process.env.CONTENTFUL_HOST,
+        contentTypeFilter: contentType => !contentType.sys.id.startsWith('page')
       },
     },
   ],
